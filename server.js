91407b727fe8f574fcba6de23c6c985e5f1450cd
Denis Myshkov <dmyshkov@perfectionlearning.com>
2022-09-16 14:44:31 -0700

    first

---
+++ server.js
@@ -0,0 +1,23 @@
+const shell = require('shelljs');
+const express = require('express');
+const app = express();
+
+let status = "open";
+
+app.get('/', (req, res) => {
+  // res.send('Hello from App Engine!');
+  if (status == "closed") {
+    res.send('Tests are already in progress.');
+  }
+  else {
+    status = "closed";
+    shell.exec('cd ../webdriver-e2e-automation; ./kek-script', () => {status = "open"});
+    res.send('Tests are running. You can find the result at http://34.168.199.237/');
+  };  
+});
+
+// Listen to the App Engine-specified port, or 8080 otherwise
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}...`);
+});
